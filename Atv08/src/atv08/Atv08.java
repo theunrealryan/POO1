3f0853ef94c311a156b1bb2a585e65dff25df687
Ryan Ricardo de Souza <140509330+theunrealryan@users.noreply.github.com>
2024-09-11 22:23:59 -0300

    Update Atv08.java

--- Atv08/src/atv08/Atv08.java
+++ Atv08/src/atv08/Atv08.java
@@ -7,7 +7,7 @@ public class Atv08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-       // Dados do triângulo retângulo
+       // Entrada de dados do triângulo retângulo (cateto oposto e adjacente)
         System.out.print("Digite o cateto oposto: ");
         double catetoOposto = scanner.nextDouble();
         while (catetoOposto <= 0) {
@@ -26,7 +26,7 @@ public class Atv08 {
         // Calculando a hipotenusa
         double hipotenusa = Math.sqrt(Math.pow(catetoOposto, 2) + Math.pow(catetoAdjacente, 2)); //método Math.sqrt (raiz quadrada) e Math.pow (potenciação)
 
-        // Dados trigonométricos
+        // Entreada de dados trigonométricos
         System.out.print("Digite um ângulo em radianos: ");
         double anguloRadianos = scanner.nextDouble();
         while (anguloRadianos < 0 || anguloRadianos > 2 * Math.PI) {
@@ -34,7 +34,7 @@ public class Atv08 {
             anguloRadianos = scanner.nextDouble();
         }
         
-        // Calculando seno, cosseno e tangente
+        // Calculando de radianos para seno, cosseno e tangente
         double seno = Math.sin(anguloRadianos);
         double cosseno = Math.cos(anguloRadianos);
         double tangente = Math.tan(anguloRadianos);
